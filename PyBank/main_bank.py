9f145a5650231a0180220db89838104e0a46f0bd
Bradley Mendolla <bradmendolla@gmail.com>
2019-06-12 22:08:04 -0400

    Progress on avg_change function

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -4,10 +4,16 @@ import csv
 budget_csv = os.path.join ("..", "Resources", "budget_data.csv")
 
 monthly_profit = []
+average_profit = []
 
-def average_change(monthly_profit):
-    return
 
+def avg_change(monthly_profit):
+    i = 0
+    for x in range(len(monthly_profit) - 1):
+        x = (int(monthly_profit[i + 1]) - int(monthly_profit[i])) / 2
+        i += 1
+        average_profit.append(x)
+    return monthly_profit
 
 
 with open(budget_csv, "r") as csvfile:
@@ -18,12 +24,14 @@ with open(budget_csv, "r") as csvfile:
     total = 0
     
     for row in csvreader:
+        months = len(row[0])
         total += int(row[1])
         
         monthly_profit.append(row[1])
 
-        
     
+    #avg_change(monthly_profit)
     
+    print(months)
     print(total)
-    print(monthly_profit)
+    
