4178cb896ddf6480d59b06cb0bdde8eb3fbfd2eb
Bradley Mendolla <bradmendolla@gmail.com>
2019-06-13 18:30:20 -0400

    finished avg_change function

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -3,35 +3,50 @@ import csv
 
 budget_csv = os.path.join ("..", "Resources", "budget_data.csv")
 
+# global variables
 monthly_profit = []
 average_profit = []
-
-
+months = []
+# function for finding average change in profit
 def avg_change(monthly_profit):
     i = 0
-    for x in range(len(monthly_profit) - 1):
-        x = (int(monthly_profit[i + 1]) - int(monthly_profit[i])) / 2
+    for value in range(len(monthly_profit) - 1):
+        value = (int(monthly_profit[i + 1]) - int(monthly_profit[i])) / 2
         i += 1
-        average_profit.append(x)
+        average_profit.append(value)
+    avg = float((sum(average_profit)) / len(average_profit))
+    print(avg)
     return monthly_profit
 
-
+# open and read csv
 with open(budget_csv, "r") as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ",")
+    # skip header
 
     header = next(csvreader)
+
+    # define total
+
     total = 0
     
     for row in csvreader:
-        months = len(row[0])
+        #total profit value of positive and negative profit and append to list
         total += int(row[1])
         
         monthly_profit.append(row[1])
+        
+        # create list of months 
+
+        months.append(row[0])
+        
+        
 
     
-    #avg_change(monthly_profit)
+    # print values as text
     
-    print(months)
-    print(total)
+    print(f"Months: " + str(len(months)))
+    print(f"Total Profit: " + str(total))
     
+    avg_change(monthly_profit)
+
