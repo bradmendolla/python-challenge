f0e1cc65e17e3267ae287c17e3c05fb18ff39777
Bradley Mendolla <bradmendolla@gmail.com>
2019-06-14 15:20:04 -0400

    finished dictionary and started work on highest_profit function

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -7,6 +7,7 @@ budget_csv = os.path.join ("..", "Resources", "budget_data.csv")
 monthly_profit = []
 average_profit = []
 months = []
+profit_dict = {}
 # function for finding average change in profit
 def avg_change(monthly_profit):
     i = 0
@@ -18,10 +19,15 @@ def avg_change(monthly_profit):
     print(avg)
     return monthly_profit
 
+def highest_profit(profit_dict):
+
+    return
+
 # open and read csv
 with open(budget_csv, "r") as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter = ",")
+
     # skip header
 
     header = next(csvreader)
@@ -32,21 +38,34 @@ with open(budget_csv, "r") as csvfile:
     
     for row in csvreader:
         #total profit value of positive and negative profit and append to list
-        total += int(row[1])
+        total += float(row[1])
         
         monthly_profit.append(row[1])
         
         # create list of months 
 
         months.append(row[0])
+    
+
+    # write directly to dictionary
+    
+with open(budget_csv, "r") as dictfile:
+    
+    
+    csvreader = csv.reader(dictfile, delimiter = ",")
+    header = next(csvreader)
+    
+    profit_dict = dict((row[0], row[1]) for row in csvreader)
         
         
 
     
     # print values as text
     
+    
     print(f"Months: " + str(len(months)))
     print(f"Total Profit: " + str(total))
     
-    avg_change(monthly_profit)
+   
+    #avg_change(monthly_profit)
 
