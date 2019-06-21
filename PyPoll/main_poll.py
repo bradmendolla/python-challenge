0fba5476e2609eda2cc37abcd062b477109b5b96
Bradley Mendolla <bradmendolla@gmail.com>
2019-06-21 14:32:24 -0400

    Finished assignment

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+import operator
 election_csv = os.path.join( "..", "Resources", "election_data.csv")
 
 
@@ -22,17 +22,27 @@ with open (election_csv, "r") as csvfile:
             li_vote +=1
         elif row[2] == "Correy":
             correy_vote +=1
-        else:
+        elif row[2 == "O'Toole"]:
             otoole_vote += 1   
+
+# create lists
+vote_list = [khan_vote, li_vote, correy_vote, otoole_vote]
+candidates = ["Khan", "Li", "Correy", "O'Toole"]
+# zip lists into dictionary 
+result = zip(candidates, vote_list)
+results_dict = dict(result)
+# find the winner
+winner = max(results_dict.items(), key=operator.itemgetter(1))[0]
+
+
     
-    
-    print(f"Total Votes: {total_vote}")
-    print(f"Khan Vote: % {(khan_vote/total_vote)*100} ({khan_vote})")
-    print(f"Li Vote: % {(li_vote/total_vote)*100} ({li_vote})")
-    print(f"Correy Vote: % {(correy_vote/total_vote)*100} ({correy_vote})")
-    print(f"O'Toole Vote: % {(otoole_vote/total_vote)*100} ({otoole_vote})")
-    print("------------------")
-    print("Winner: Khan")
+print(f"Total Votes: {total_vote}")
+print(f"Khan Vote: % {(khan_vote/total_vote)*100} ({khan_vote})")
+print(f"Li Vote: % {(li_vote/total_vote)*100} ({li_vote})")
+print(f"Correy Vote: % {(correy_vote/total_vote)*100} ({correy_vote})")
+print(f"O'Toole Vote: % {(otoole_vote/total_vote)*100} ({otoole_vote})")
+print("------------------")
+print(f"Winner: {winner}")
 
 f = open("analysis.txt", "w")
 f.write(f"Total Votes: {total_vote}\n")
@@ -41,6 +51,7 @@ f.write(f"Li Vote: % {(li_vote/total_vote)*100} ({li_vote})\n")
 f.write(f"Correy Vote: % {(correy_vote/total_vote)*100} ({correy_vote})\n")
 f.write(f"O'Toole Vote: % {(otoole_vote/total_vote)*100} ({otoole_vote})\n")
 f.write("------------------\n")
-f.write("Winner: Khan")
+f.write(f"Winner: {winner}")
+
 
 f.close
